e35775d97fb98c2030a812c397034467d6a30294
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-19 22:23:28 +0530

    Update until_last_zero.py

--- until_last_zero.py
+++ until_last_zero.py
@@ -1,6 +1,17 @@
+import collections
 n = int(input(""))
 s = input("")
 k = s.rfind('0',0,n)
-for i in range(k):
-    if(s[i] != '0'):
-        print(s[i],end = "")
+c = 0
+freq = collections.Counter(s)
+if(freq['0'] == 1):
+    for i in range(0,k):
+        print(s[i],end="")
+else:
+    for i in range(k):
+        if(s[i] != '0'):
+            if(c ==0):
+                c+=1
+                print(s[i],end = "")
+            else:
+                print("",s[i],end = "")
