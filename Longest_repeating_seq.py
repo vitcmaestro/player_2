d545edb5ded363b43616b82d5d98954977a80042
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-20 11:42:50 +0530

    Create Longest_repeating_seq.py

---
+++ Longest_repeating_seq.py
@@ -0,0 +1,10 @@
+import collections
+n = int(input())
+mem = list(map(str,input().split()))
+dic = dict(collections.Counter(mem))
+maxer = 0
+for x,y in dic.items():
+    if(y>maxer):
+        ans = x
+        maxer = y
+print(maxer)
