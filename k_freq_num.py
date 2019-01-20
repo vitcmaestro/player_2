1fad2f11035e9ca3e1a6be3b70384418a7b6400b
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-20 11:38:19 +0530

    Update k_freq_num.py

--- k_freq_num.py
+++ k_freq_num.py
@@ -1,5 +1,8 @@
 import collections
-n,k = map(int,input().split())
-mem = list(map(int,input().split()))
-dic = collections.Counter(mem)
-print(dic[k])
+n = list(map(int,input().split()))
+mem = list(map(str,input().split()))
+dic = dict(collections.Counter(mem))
+k = n[len(n)-1]
+for x,y in dic.items():
+    if(y == k):
+        print(x)
